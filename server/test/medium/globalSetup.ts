a7353af0132cad64a4fd0f07a2feb6308dae8d98
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-07-31 00:24:18 +0200

    bump postgis tag

--- server/test/medium/globalSetup.ts
+++ server/test/medium/globalSetup.ts
@@ -35,7 +35,7 @@ const globalSetup = async (): Promise<() => Promise<void>> => {
   const username = 'postgres';
   const password = 'postgres';
 
-  const postgresContainer = await new GenericContainer('postgis/postgis:18')
+  const postgresContainer = await new GenericContainer('postgis/postgis:18-3.6')
     .withExposedPorts(5432)
     .withEnvironment({
       POSTGRES_PASSWORD: password,
