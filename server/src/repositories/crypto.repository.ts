a494cb5f303d8d657a1f0e6d749f3beb2d52917b
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-01 22:20:00 +0200

    use xxh 128

--- server/src/repositories/crypto.repository.ts
+++ server/src/repositories/crypto.repository.ts
@@ -5,7 +5,7 @@ import { randomUUID } from 'node:crypto';
 @Injectable()
 export class CryptoRepository {
   xxHash(contents: Uint8Array): string {
-    return xxh3.Xxh3.withSeed().update(contents).digest().toString(16).padStart(16, '0');
+    return xxh3.xxh128(contents).toString(16).padStart(32, '0');
   }
 
   uuid(): string {
