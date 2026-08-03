41068f9eab2c19ba8920cf31118a19ba999d34f3
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-03 23:52:57 +0200

    fix test

--- server/src/services/activity.service.spec.ts
+++ server/src/services/activity.service.spec.ts
@@ -180,7 +180,7 @@ describe('ActivityService', () => {
       await makeService().handleActivityParseQueueAll({ force: true });
 
       expect(queueAll).toHaveBeenCalledWith([
-        { name: JobName.ActivityParse, data: { id: 'a', force: true, source: 'backfill' } },
+        { name: JobName.ActivityParse, data: { id: 'a', force: true } },
       ]);
       expect(getIdsToParse.mock.calls[0][0]).toMatchObject({ force: true });
     });
