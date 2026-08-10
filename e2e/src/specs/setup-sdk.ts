6a72b02737f586b502f5c032ba62116c03c42380
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-10 20:04:57 +0200

    fix e2e inits

--- e2e/src/specs/setup-sdk.ts
+++
@@ -1,9 +0,0 @@
-import { defaults } from '@kondis/sdk';
-
-const serverUrl = process.env.KONDIS_E2E_SERVER_URL;
-
-if (!serverUrl) {
-  throw new Error('KONDIS_E2E_SERVER_URL must be set');
-}
-
-defaults.baseUrl = serverUrl;
