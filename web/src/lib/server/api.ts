30dcc1da47c38048cc7cbdf33bfde84d90f10fa3
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-10 22:23:37 +0200

    cleanup

--- web/src/lib/server/api.ts
+++ web/src/lib/server/api.ts
@@ -1,6 +1,6 @@
-import { env } from '$env/dynamic/private';
+import { env } from "$env/dynamic/private";
 
 export function apiUrl(path: string): URL {
-  const apiBase = env.KONDIS_API_URL ?? 'http://localhost:2293';
-  return new URL(path.replace(/^\//, ''), `${apiBase.replace(/\/$/, '')}/`);
+  const apiBase = env.KONDIS_API_URL ?? "http://localhost:2293";
+  return new URL(path.replace(/^\//, ""), `${apiBase.replace(/\/$/, "")}/`);
 }
