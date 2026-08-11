48e6076b693e7f814b0a96ca4ed1c543b242ea0f
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-11 13:03:37 +0200

    cleanup

--- e2e/src/specs/server/api/activities.e2e-spec.ts
+++ e2e/src/specs/server/api/activities.e2e-spec.ts
@@ -30,7 +30,7 @@ describe('GET /activities', () => {
     const activity = await e2e.createActivity(orsaTcxFixturePath, 'application/xml');
 
     expect(activity.sport).toBe('other');
-    expect(activity.startedAt).toBe('2013-01-13T09:17:34.000Z');
+    expect(activity.startedAt).toBe('2013-01-13T09:16:35.000Z');
     expect(activity.elapsedTime).toBe(10_962);
     expect(activity.movingTime).toBe(10_962);
     expect(activity.distance).toBeCloseTo(29_823.963165283203, 3);
