f1fbf6f288f86ab8a8ce50b679e6f520598b3efa
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-19 00:13:18 +0200

    set vite allowedhosts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
+    allowedHosts: process.env.KONDIS_VITE_ALLOWED_HOSTS === "true",
     watch:
       process.env.KONDIS_VITE_USE_POLLING === "true"
         ? { usePolling: true, interval: 250 }
