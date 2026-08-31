2fc6fa432aa118e4ebf0d92ace054fd4bcffd26c
Jonathan Jogenfors <jonathan@jogenfors.se>
2026-08-31 14:24:43 +0200

    new api site

--- sites/api.kondis.org/legacy/src/lib/components/MethodBadge.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import type { HttpMethod } from "$lib/openapi";
-
-  let { method }: { method: HttpMethod } = $props();
-</script>
-
-<span class="method-badge method-{method}">{method}</span>
